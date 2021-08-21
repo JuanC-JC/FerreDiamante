617c62c04786367e48e8f1994129048d76a93b15
JuanC-JC <juancchaparro97@gmail.com>
2021-08-21 15:48:47 -0500

    add first styles

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './styles/intro.scss';
+import './styles/data.scss';
+import './styles/info.scss'
+
+
+// const data = document.querySelector('.data')
+
+// console.log(data)
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output:{
+    path: path.resolve(__dirname,'dist'),
+    filename:'bundle.js',
+    clean: true,
+  },
+  resolve:{
+    extensions: ['.js']
+  },
+  module:{
+    rules:[
+      {
+        test: /\.(s?css)$/i,
+        use:[
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          'css-loader',
+          'sass-loader']
+      },
+      {
+        test:/\.js$/i,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.(png|jpg|svg)$/i,
+        type: 'asset',
+      }
+    ]
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename:'index.html',
+      inject: true,
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
+  ]
+}
